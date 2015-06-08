052ac145819630c6367e5b28a6b76a4c288a3a3a
Gubin, Maksim <maksim.gubin@oxagile.com>
2015-06-08 14:58:46 +0300

    task1 fix path

--- src/Workshop/Bundle/ProductBundle/Entity/Product.php
+++ src/Workshop/Bundle/ProductBundle/Entity/Product.php
@@ -216,7 +216,7 @@ class Product
     {
         // the absolute directory path where uploaded
         // documents should be saved
-        return __DIR__.'/../../../../../../web/'.$this->getUploadDir();
+        return __DIR__.'/../../../../../web/'.$this->getUploadDir();
     }
 
     protected function getUploadDir()
